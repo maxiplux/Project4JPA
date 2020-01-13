09758bc44a00d17c412b7171b534ddcd4b7de40b
Juan Mosquera <maxiplux@gmail.com>
2020-01-12 21:35:39 -0500

    add support to lab 13 . Final course.

---
+++ src/main/java/com/packt/webstore/config/Interceptor.java
@@ -0,0 +1,29 @@
+package com.packt.webstore.config;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Interceptor extends HandlerInterceptorAdapter {
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+
+        String displayMsg;
+
+        if(request.getUserPrincipal()!=null) {
+            if(request.isUserInRole("ROLE_ADMIN")) {
+                displayMsg="Full Authorization";
+            }else {
+                displayMsg="Basic Authorization";
+            }
+        }else {
+            displayMsg="No User Authenticated yet!!!";
+        }
+
+        modelAndView.getModelMap().addAttribute("msg",displayMsg);
+    }
+
+}
